2d2800c9d6655ef7726dc5f94110acbe6669fb09
William Mooney Sloneker <wmsloneker@gmail.com>
2019-07-31 20:03:54 -0400

    Added a 'start' button to the HTML; initial build of the random Pokemon generator in JS

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,14 +1,13 @@
 // Define word options for the hangman game
-var Pokemon = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Eevee"];
+var pokemon = ["BULBASAUR", "CHARMANDER", "SQUIRTLE", "PIKACHU", "EEVEE"];
 
 // Define variables that will record values for the game
 var wins = 0;
 var losses = 0;
 var guesses = 10;
-var lettersGuessed = "";
+var lettersGuessed = [""];
 
 // Define variables that hold references to HTML elements that will display values
-
 var instructionsText = document.getElementById("instructions-text");
 var pokemonLabelText = document.getElementById("Pokemon-label-text");
 var currentPokemonText = document.getElementById("current-Pokemon-text");
@@ -16,3 +15,10 @@ var guessesLeftText = document.getElementById("guesses-left-text");
 var lettersGuessedText = document.getElementById("letters-guessed-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
+
+//Initiate the game with press of button
+function randomPokemon (pokemon) {
+    // Assign a random Pokemon to be guessed
+    return pokemon[Math.floor(Math.random() * pokemon.length)];
+}
+console.log(randomPokemon(pokemon));
